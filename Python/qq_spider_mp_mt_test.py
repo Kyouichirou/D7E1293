031ce3a1bceda37670074e1dcb45f84a44c48916
Kaga Kyouichirou <64763483+Kyouichirou@users.noreply.github.com>
2021-06-17 11:24:58 +0200

    Update qq_spider_mp_mt_test.py

--- Python/qq_spider_mp_mt_test.py
+++ Python/qq_spider_mp_mt_test.py
@@ -1,20 +1,28 @@
 from bs4 import BeautifulSoup as bFs
 import requests, time
 from multiprocessing import Pool, cpu_count
-
+import aiohttp, asyncio
+from concurrent.futures import ProcessPoolExecutor as pPe
+import concurrent.futures as cf
+import random_ua
+import threading
 
 def get_dom(url):
     headers = {
         'Connection': 'keep-alive',
         'DNT': '1',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.88 Safari/537.36',
-        'Sec-Fetch-Dest': 'document',
+        'User-Agent': random_ua.rdua(),
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Accept-Language': 'en-US,en;q=0.9',
+        "Accept - Encoding": "gzip, deflate, br",
+        "Referer": 'https://www.qq.com'
     }
-    r = requests.get(url, headers=headers, timeout=(8, 8))
-    return bFs(r.content.decode('gbk'), 'html.parser') if r.status_code == 200 else None
+    try:
+        r = requests.get(url, headers=headers, timeout=(8, 8))
+        return bFs(r.content.decode('gbk'), 'html.parser') if r.status_code == 200 else None
+    except requests.Timeout:
+        print('timeout: ', url)
+        return None
 
 
 def get_content(url):
@@ -60,6 +68,55 @@ def multi_core():
             f.writelines(m + '\n')
 
 
+def multi_core_pool():
+    arr = get_list()
+    with pPe() as pool:
+        mp = pool.map(get_content, arr)
+    with open(r'C:\Users\Lian\Desktop\test_news.txt', mode='w', encoding='utf-8') as f:
+        for m in mp:
+            f.writelines(m + '\n')
+
+
+def multi_thread_pool():
+    arr = get_list()
+    with cf.ThreadPoolExecutor(max_workers=4) as pool:
+        mp = [pool.submit(get_content, url) for url in arr]
+        with open(r'C:\Users\Lian\Desktop\test_news.txt', mode='w', encoding='utf-8') as f:
+            for m in cf.as_completed(mp):
+                    f.writelines(m.result() + '\n')
+
+
+def multi_thread():
+    arr = get_list()
+    mp = [threading.Thread(get_content,args=(url,)) for url in arr]
+    for m in mp:
+        m.start()
+    for m in mp:
+        m.join()
+
+async def async_craw(url, f):
+    timeout = aiohttp.ClientTimeout(total=8)
+    async with aiohttp.ClientSession(timeout=timeout) as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                result = await resp.text(encoding='gbk')
+                dom = bFs(result, 'html.parser')
+                c = dom.find('div', class_='content-article')
+                if c:
+                    f.writelines(c.text.strip() + '\n')
+
+
+def start_async():
+    arr = get_list()
+    f = open(r'C:\Users\Lian\Desktop\test_news.txt', mode='w', encoding='utf-8')
+    loop = asyncio.get_event_loop()
+    tasks = [
+        loop.create_task(async_craw(url, f))
+        for url in arr]
+    loop.run_until_complete(asyncio.wait(tasks))
+    f.close()
+
+
 def normal():
     arr = get_list()
     if not arr:
@@ -74,5 +131,5 @@ def normal():
 
 if __name__ == '__main__':
     start = time.time()
-    normal()
+    start_async()
     print(time.time() - start)
