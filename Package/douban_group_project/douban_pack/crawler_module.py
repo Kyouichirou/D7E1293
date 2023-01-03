775b1d3d8a92b4723a85278248f0b987dcb27a50
Kaga Kyouichirou <64763483+Kyouichirou@users.noreply.github.com>
2023-01-03 11:16:42 +0800

    Update crawler_module.py

--- Package/douban_group_project/douban_pack/crawler_module.py
+++ Package/douban_group_project/douban_pack/crawler_module.py
@@ -53,7 +53,7 @@ class Crawler:
 
     def request(self, url: str, referer: str) -> str:
         try:
-            print(f'get data from: {url}')
+            print(f'[get data from]: {url}')
             if self.__visited_times == self.__random_count:
                 self.__random_count = random.randint(15, 35)
                 self._visited_state = False
@@ -65,23 +65,20 @@ class Crawler:
 
             if self._visited_state:
                 self._headers['Referer'] = referer
-
             self.__visited_times += 1
-
             r = self._session.get(url, headers=self._headers, timeout=(33, 33))
             code = r.status_code
             self._state_code = code
             self._request_times += 1
-
             time.sleep(random.uniform(3.25, 10.25))
-
             if code == 200:
                 # decode支持的一个额外参数, 默认是严格模式strict
                 # errors='ignore'
                 # 支持检查的额外的编码参数, 默认为strict类型
                 return r.content.decode('utf-8')
-            self._anti_spider = True
-            _logger.warning(f'fail: no 200, {url}, {referer}, {code}')
+            if not (code == 404 and '&type=new' in url):
+                self._anti_spider = True
+            _logger.warning(f'[fail]: no 200, {url}, {referer}, {code}')
         except Exception as error:
             _logger.capture_except(error)
 
@@ -92,14 +89,16 @@ class Crawler:
     def exit_crawler(self):
         if not self._close_state:
             self._session.close()
-            self._close_state = True
             self._session = None
+            self._close_state = True
 
     def reset_connect(self):
+        if self._request_times == 0:
+            return
         self.exit_crawler()
-        print('switch to new agent....')
-        time.sleep(random.uniform(0.55, 5.25))
         self.__visited_times = 0
+        time.sleep(random.uniform(0.55, 5.25))
+        print('[info]: switch to new agent....')
         self._session = requests.session()
         self._headers['Sec-Fetch-Site'] = 'none'
         self._headers['User-Agent'] = self._random_ua.get_ua()
