5c32e5cf86b1d393eeb1051a07406e366ebafe09
Kaga Kyouichirou <64763483+Kyouichirou@users.noreply.github.com>
2023-06-13 15:26:47 +0800

    Delete Package/douban_group_project directory

--- Package/douban_group_project/douban_pack/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-@name: douban spider
-@author: HLA
-@version: 1.0
-@github: https://github.com/Kyouichirou
-@license: MIT
-@update: 2022-12-30
-@description: spider for douban group
-"""
--- Package/douban_group_project/douban_pack/utils/voice_module.py
+++
@@ -1,7 +0,0 @@
-import win32com.client as win_client
-
-
-def notification_voice(contents):
-    # text => speak
-    speak = win_client.Dispatch("SAPI.SpVoice")
-    speak.Speak(contents)
